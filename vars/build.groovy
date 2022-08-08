a5c0ef24411bbbd7f48cbe44f1cc00015415d4ff
sandeep4642 <sandeep.click100@gmail.com>
2022-08-08 15:21:47 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,6 +1,7 @@
 def mavenbuild() {
    withSonarQubeEnv('sonar') {
       def BRANCH_NAME = utils.getbranch()
+   bat "mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install org.jacoco:jacoco-maven-plugin:report"   
    bat "mvn sonar:sonar -Dsonar.branch.name=${BRANCH_NAME}"
    }
 }
