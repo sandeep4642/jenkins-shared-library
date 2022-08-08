13efec615c00149d9bd6ac97898a7c011c55dbf7
sandeep4642 <sandeep.click100@gmail.com>
2022-08-08 15:17:15 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,6 +1,6 @@
 def mavenbuild() {
    withSonarQubeEnv('sonar') {
-      def BRANCH_NAME = albutils.getbranch()
+      def BRANCH_NAME = utils.getbranch()
    bat "mvn sonar:sonar -Dsonar.branch.name=${BRANCH_NAME}"
    }
 }
