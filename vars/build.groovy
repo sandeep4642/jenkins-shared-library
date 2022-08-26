d78711ef17a86b3ac46953c95a208bbe437595d7
sandeep4642 <sandeep.click100@gmail.com>
2022-08-26 17:10:08 +0530

    maven

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,7 +1,20 @@
 def mavenbuild() {
+   echo "Building the package"
+   sh 'mvn clean install package'
+}
+
+
+def mavensonarbuild() {
    withSonarQubeEnv('sonar') {
       def BRANCH_NAME = utils.getbranch()
    bat "mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install org.jacoco:jacoco-maven-plugin:report"   
    bat "mvn sonar:sonar -Dsonar.branch.name=${BRANCH_NAME}"
    }
+}
+
+def buildDockerTaggedImage(Registry, RegistryRepo, Dockerfile, Tag) {
+   def imageURI = utils.getImageURI(Registry, RegistryRepo, Tag)
+
+   dockerImage = docker.build(imageURI, "-f $Dockerfile .")
+   return dockerImage
 }
--- vars/utils.groovy
+++ vars/utils.groovy
@@ -3,3 +3,10 @@ def getbranch() {
   branch = (branch == null || branch == "null") ? "master": branch
   return branch
 }
+
+def getImageURI(Registry, RegistryRepo, Tag){
+  def imageURI = Registry + "$RegistryRepo:$Tag"
+
+  echo $imageURI
+  return imageURI
+}
